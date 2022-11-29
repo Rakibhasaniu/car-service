7cff5a1a5ef7d983b995bfe9e541fbf0277b1def
toukircse77 <toukircse777@gmail.com>
2022-11-29 15:59:04 +0600

    Load all categories

--- src/Pages/Car Categori/CarCategori.js
+++ src/Pages/Car Categori/CarCategori.js
@@ -56,58 +56,6 @@ const CarCategori = () => {
                         )
                     }
 
-                    {/* <div className="max-w-lg p-4 shadow-md dark:bg-gray-900 dark:text-gray-100 rounded-lg">
-                        <div className="flex justify-between pb-4 border-bottom">
-                            <div className="flex items-center">
-                                <a rel="noopener noreferrer" href="#" className="mb-0 capitalize dark:text-gray-100">Replles<span className='text-amber-600'> Tesla Car</span></a>
-                            </div>
-                            <a rel="noopener noreferrer" href="#">Replles Car 3</a>
-                        </div>
-                        <div className="space-y-4">
-                            <div className="space-y-2">
-                                <img src="https://source.unsplash.com/random/480x360/?4" alt="" className="block object-cover object-center w-full rounded-md h-72 dark:bg-gray-500" />
-                                <div className="flex items-center text-xs justify-between">
-                                    <span>Uplode Date : 10/02/2022</span>
-                                    <span>5 day ago</span>
-                                </div>
-                            </div>
-                            <div className="space-y-2">
-                                <a rel="noopener noreferrer" href="#" className="block">
-                                    <h3 className="text-xl font-semibold dark:text-violet-400">Facere ipsa nulla corrupti praesentium pariatur architecto</h3>
-                                </a>
-                                <p className="leading-snug dark:text-gray-400">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat, excepturi. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat, excepturi.</p>
-                                <div className="flex justify-center mt-3" >
-                                    <button className="btn md:px-12 btn-primary">viewss all cars</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="max-w-lg p-4 shadow-md dark:bg-gray-900 dark:text-gray-100 rounded-lg">
-                        <div className="flex justify-between pb-4 border-bottom">
-                            <div className="flex items-center">
-                                <a rel="noopener noreferrer" href="#" className="mb-0 capitalize dark:text-gray-100">Replles<span className='text-amber-600'> Tesla Car</span></a>
-                            </div>
-                            <a rel="noopener noreferrer" href="#">Replles Car 3</a>
-                        </div>
-                        <div className="space-y-4">
-                            <div className="space-y-2">
-                                <img src="https://source.unsplash.com/random/480x360/?4" alt="" className="block object-cover object-center w-full rounded-md h-72 dark:bg-gray-500" />
-                                <div className="flex items-center text-xs justify-between">
-                                    <span>Uplode Date : 10/02/2022</span>
-                                    <span>5 day ago</span>
-                                </div>
-                            </div>
-                            <div className="space-y-2">
-                                <a rel="noopener noreferrer" href="#" className="block">
-                                    <h3 className="text-xl font-semibold dark:text-violet-400">Facere ipsa nulla corrupti praesentium pariatur architecto</h3>
-                                </a>
-                                <p className="leading-snug dark:text-gray-400">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat, excepturi. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat, excepturi.</p>
-                                <div className="flex justify-center mt-3" >
-                                    <button className="btn md:px-12 btn-primary">viewss all cars</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div> */}
 
 
                 </div>
